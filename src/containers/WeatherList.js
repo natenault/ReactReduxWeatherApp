ef3b8ca06d9322f4984c90df252b33727933cba6
Nate Nault <nate@nathannault.com>
2017-07-18 15:37:08 +0800

    Create WeatherList and Chart components and populate data

---
+++ src/containers/WeatherList.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Chart from '../components/Chart';
+
+class WeatherList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.renderWeather = this.renderWeather.bind(this);
+  }
+
+  renderWeather(cityData) {
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+
+    return (
+      <tr key={cityData.city.id}>
+        <td>{cityData.city.name}</td>
+        <td><Chart data={temps} color="red" units="K" /></td>
+        <td><Chart data={pressures} color="green" units="hPa" /></td>
+        <td><Chart data={humidities} color="blue" units="%" /></td>
+      </tr>
+    );
+  }
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps({ weather }) {
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
